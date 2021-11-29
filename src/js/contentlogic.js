96b7d0a4e7ecb8439f49c9940ca634a65594d1da
GitLumbo <grumpysh01@gmail.com>
2021-11-29 11:07:44 +0900

    ver 0.0.1

---
+++ src/js/contentlogic.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+  let src = sessionStorage.getItem("src"); // 세션 스토리지에서 클릭한 주소 가져옴
+  document.querySelector("iframe").onload = function () {
+    document.querySelector("#wait-over").remove(); // '불러오는 중' 메세지 제거
+  };
+
+  document.querySelector("iframe").src = src; // iframe 주소 설정
+  document.querySelector("#back").addEventListener("click", () => {
+    window.location.href = "popup.html"; // 뒤로가기 버튼
+  });
+
+  document.querySelector("#newtab").addEventListener("click", () => {
+    chrome.tabs.create({ url: src }); // 새탭에서 열기 버튼
+  });
+};
---
+++ src/js/mainlogic.js
@@ -0,0 +1,59 @@
+// 공지시항 누르면 공지사항으로 이동
+window.onload = function () {
+  document.querySelector("#title").addEventListener("click", () => {
+    chrome.tabs.create({ url: "https://www.inha.ac.kr/kr/950/subview.do" });
+  });
+};
+// 공지사항 테이블 주소
+const att_url = new URL("https://www.inha.ac.kr/bbs/kr/8/artclList.do");
+req = new XMLHttpRequest();
+req.open("GET", att_url);
+req.send();
+getAttTable = function () {
+  if (req.readyState == "4") {
+    clearInterval(timer);
+
+    // 응답 html
+    const responsedoc = req.responseText;
+    // node 추출을 위해 div 생성
+    let box = document.createElement("div");
+    box.innerHTML = responsedoc;
+    let table = box.querySelector("table"); // 공지사항 테이블
+    let body = table.querySelector("tbody");
+    let all = body.querySelectorAll("tr"); // 공지사항 row
+
+    let createNode = document.createElement("ul"); // popup.html에 보여질 ul
+
+    let hr = document.createElement("hr"); // 맨 윗줄 생성
+    createNode.appendChild(hr);
+
+    // 모든 row에 대한 for문
+    for (let el of all) {
+      // 헤드라인 안붙은 것만 뽑아냄
+      if (!el.classList.length) {
+        let main_el = el.querySelector("a"); // 하나의 row
+        let title = main_el.innerText; // 제목
+        let link = new URL(main_el.href);
+        link = att_url.origin + link.pathname; // 링크
+
+        let node = document.createElement("li"); // popup.html의 ul에 들어갈 li
+        node.classList.add("one_tr"); // css
+        node.innerText = title.replace(/\n/g, ""); // 제목 공백 모두 제거
+        // 클릭시 새탭으로 이동
+        node.onclick = function () {
+          sessionStorage.setItem("src", link);
+          window.location.href = "content.html";
+          // chrome.tabs.create({ url: link });
+        };
+        let hr = document.createElement("hr"); // 구분선 표시
+
+        createNode.appendChild(node); // li 추가
+        createNode.appendChild(hr); // 구분선 추가
+      }
+    }
+
+    document.querySelector("#waiting").remove(); // '불러오는 중' 메세지 제거
+    document.querySelector("#app").appendChild(createNode); // 최종적으로 #app에 붙임
+  }
+};
+timer = setInterval(getAttTable, 100);
